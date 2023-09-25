305621ec8242cf9162e98d9d5a0a6ce238665577
pardaboyevkarim <pardaboyevkarim9@gmail.com>
2023-09-25 20:01:30 +0500

    add

---
+++ main.js
@@ -0,0 +1,25 @@
+let elInputOne = document.querySelector(".form-input");
+let elResult = document.querySelector(".result");
+let elResultt = document.querySelector(".resultt");
+let elbtrest = document.querySelector(".resett");
+let elSelect = document.querySelector(".select");
+
+elInputOne.addEventListener("keyup", function () {
+  let x = +elSelect.value;
+  let a = +elInputOne.value;
+  let b = a / x;
+  if (x == 12500) {
+    elResultt.textContent = a + ": SO'M";
+    elResult.textContent = b.toFixed(2) + ": EN ";
+  } else if (x == 13050) {
+    elResultt.textContent = a + ": SO'M";
+    elResult.textContent = b.toFixed(2) + ": YEV ";
+  } else if (x == 170) {
+    elResultt.textContent = a + ": SO'M";
+    elResult.textContent = b.toFixed(2) + ": RU ";
+  }
+});
+elbtrest.addEventListener("click", function () {
+  elResult.textContent = 0;
+  elResultt.textContent = 0;
+});
